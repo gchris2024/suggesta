e0956e9fe6df6f71efa9f2136b8e4cf0a4de4798
gchris2024 <gchris3586@gmail.com>
2026-04-22 11:18:39 +0200

    page routing scaffolding

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Recommendations from "./pages/Recommendations";
+import Saved from "./pages/Saved";
+
 function App() {
   return (
-    <>
-      <h1 className="font-bold text-xl"> Hello World! </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/saved" element={<Saved />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
